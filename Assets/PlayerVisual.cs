813b3da7c236d35caa16947829f2804398a25287
AndreasMarcato <110482371+AndreasMarcato@users.noreply.github.com>
2024-01-20 17:20:22 +0200

    player animator
    added player animator and refactored input to use the animations
    also fixed the prefab

---
+++ Assets/PlayerVisual.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVisual : MonoBehaviour
+{
+    private Animator _animator;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+    }
+
+    public void StartWalking() => _animator.SetTrigger("StartWalking");
+    public void StopWalking() => _animator.SetTrigger("StopWalking");
+}
