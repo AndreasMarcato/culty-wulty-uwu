2440998092d7b242414b12b2315ccc0ceefae161
Izmaost <118356393+Izmaost@users.noreply.github.com>
2024-01-25 21:09:00 +0100

    aaaaaaaaaaaaaaaa

--- Assets/Scripts/Ending menu.cs
+++ Assets/Scripts/Ending menu.cs
@@ -10,7 +10,10 @@ public class Endingmenu : MonoBehaviour
     {
         StartCoroutine(Ending());
     }
- 
+    private void Start()
+    {
+        END();
+    }
     private IEnumerator Ending()
     {
       
